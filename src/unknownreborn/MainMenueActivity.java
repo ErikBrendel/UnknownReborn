3253c496019be4b3852e36fd09e37d7f3dcb3c23
Erik Brendel <Erik Brendel@ErikBrendel-PC>
2015-02-17 22:35:22 +0100

    mainMenue

--- src/unknownreborn/MainMenueActivity.java
+++ src/unknownreborn/MainMenueActivity.java
@@ -15,13 +15,13 @@ import util.ImageLoader;
  * @author Erik Brendel
  */
 public class MainMenueActivity extends GameActivity {
+
     public MainMenueActivity(ActivityManager manager) {
         super(manager);
     }
 
     private BufferedImage bgImage = null;
 
-    
     @Override
     public void render(Graphics2D g, int width, int height) {
         if (!(bgImage.getWidth() == width && bgImage.getHeight() == height)) { //sollte nur das erste mal sein
@@ -31,8 +31,42 @@ public class MainMenueActivity extends GameActivity {
         g.drawImage(bgImage, 0, 0, null);
     }
 
+    int selectedButton = 0;
+    final int maxSelectedButton = 2;
+
     @Override
     public void onKeyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_W:
+                selectedButton--;
+                if (selectedButton < 0) {
+                    selectedButton = maxSelectedButton;
+                }
+                break;
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_S:
+                selectedButton++;
+                if (selectedButton > maxSelectedButton) {
+                    selectedButton = 0;
+                }
+                break;
+            case KeyEvent.VK_ENTER:
+            case KeyEvent.VK_SPACE:
+                launchButtonEvent(selectedButton);
+                break;
+            case KeyEvent.VK_ESCAPE:
+                launchButtonEvent(maxSelectedButton); //last button--> exit --> exits game
+
+        }
+        System.out.println(selectedButton);
+    }
+
+    private void launchButtonEvent(int buttonID) {
+        switch (buttonID) {
+            case maxSelectedButton:
+                System.exit(0);
+        }
     }
 
     @Override
@@ -52,5 +86,5 @@ public class MainMenueActivity extends GameActivity {
     public void onExit() {
         bgImage = null;
     }
-    
+
 }
