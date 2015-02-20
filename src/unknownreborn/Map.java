ef138d65261299a5e1baab0692609922ee3b6a22
Erik Brendel <Erik Brendel@ErikBrendel-PC>
2015-02-20 17:30:48 +0100

    MapLoading

--- src/unknownreborn/Map.java
+++ src/unknownreborn/Map.java
@@ -5,7 +5,14 @@
  */
 package unknownreborn;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 
 /**
  *
@@ -14,11 +21,22 @@ import java.io.InputStream;
 public class Map {
     public static Map loadMapFromResources(String blankFileName) {
         InputStream fileStream = Map.class.getClassLoader().getResourceAsStream("/maps/" + blankFileName + ".tmx");
-        fileStream.
-        String fileContent = "";
-        return new Map(fileContent);
+        //Scanner s = new Scanner(fileStream).useDelimiter("\\A");
+        //String fileContent = s.hasNext() ? s.next() : "";
+        return new Map(fileStream);
     }
-    public Map(String rawXMLInput) {
-        
+    public Map(InputStream in) {
+        try {
+            SAXBuilder builder = new SAXBuilder();
+            Document document = (Document) builder.build(in);
+            rootElement = document.getRootElement();
+            
+            
+            
+            
+        } catch (JDOMException | IOException ex) {
+            Logger.getLogger(Map.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+    private Element rootElement;
 }
